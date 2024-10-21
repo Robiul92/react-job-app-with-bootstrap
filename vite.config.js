3a280edd5258eae0e26d40a796e3da4683112a55
Robiul92 <robiullawal312@gmail.com>
2024-10-21 10:03:05 +0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Change this to the port you want
+    host: '0.0.0.0', // This allows access from the local network; use 'localhost' if you only need local access
+  }
+})
