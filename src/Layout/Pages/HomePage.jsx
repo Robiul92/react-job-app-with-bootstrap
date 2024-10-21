05be721cb927db3f5629360b80ec1cf72268617d
Robiul92 <robiullawal312@gmail.com>
2024-10-21 14:37:26 +0600

    react bootstrap added

--- src/Layout/Pages/HomePage.jsx
+++ src/Layout/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+
 
 const HomePage = () => {
   return (
-    <div>
-      Hello I am Home
+    <div className='d-flex justify-content-center'>
+      <Button variant="outline-warning" className='text-center btn btn-primary'>Im Button</Button>
     </div>
   )
 }
