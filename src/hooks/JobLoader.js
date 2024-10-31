b97d9306cacc75b383952821ff21023660782c46
Robiul92 <robiullawal312@gmail.com>
2024-10-31 12:38:46 +0600

    fetch replaced by axion in custom hooks

--- src/hooks/JobLoader.js
+++ src/hooks/JobLoader.js
@@ -1,13 +1,15 @@
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 // JobLoader.js
 export const JobLoader = async ({ params }) => {
     
-    const res = await fetch(`/api/jobs/${params.id}`);
-    if (!res.ok) {
-      throw new Error("Failed to load job data.");
-    }
-    const data = await res.json();
+    try {
+      const res = await axios.get(`/api/jobs/${params.id}`);
+   const data = await res.data;
     return data;
+    } catch (error) {
+      throw new Error("Failed to load job data."); 
+    }
   };
   
